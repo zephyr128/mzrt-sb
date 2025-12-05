9d389fc2808965d616bf21e1d83427b1596bf031
Nenad Prahovljanovic <prahovljanovic.n@gmail.com>
2025-12-05 18:40:00 +0100

    Add CacheKeys

---
+++ Demo/Demo/Services/Cache/CacheKey.swift
@@ -0,0 +1,12 @@
+//
+//  CacheKey.swift
+//  Demo
+//
+//  Created by Nenad Prahovljanovic on 5. 12. 2025..
+//
+
+enum CacheKey {
+    static let matches = "Matches"
+    static let competitions = "Competitions"
+    static let sports = "Sports"
+}
