da05aec18e4113ae64fa44aba095d56c7c1b05b3
Nenad Prahovljanovic <prahovljanovic.n@gmail.com>
2025-12-05 16:01:20 +0100

    Add initial Xcode project

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Nenad Prahovljanovic on 5. 12. 2025..
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo/DemoTests/DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Nenad Prahovljanovic on 5. 12. 2025..
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
