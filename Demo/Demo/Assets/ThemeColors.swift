dec3f8fcbdbf267d91d5d6904da6716711a16e76
Nenad Prahovljanovic <prahovljanovic.n@gmail.com>
2025-12-09 14:44:21 +0100

    Add ThemeColor

---
+++ Demo/Demo/Assets/ThemeColors.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeColors.swift
+//  Demo
+//
+//  Created by Nenad Prahovljanovic on 9. 12. 2025..
+//
+
+import SwiftUI
+
+// MARK: - App Theme Colors
+
+extension Color {
+    
+    // MARK: - Primary Colors
+    static let primaryColor = Color.yellow
+    static let primaryText = Color.white
+    static let secondaryText = Color.gray
+    
+    // MARK: - Background Colors
+    static let background = Color(red: 28/255, green: 31/255, blue: 33/255)
+    
+    // MARK: - Accent / Secondary Colors
+    static let secondaryColor = Color(.systemGray5)
+    static let accentColor = Color.teal
+    
+    // MARK: - Borders & Shadows
+    static let borderGray = Color.white.opacity(0.3)
+}
